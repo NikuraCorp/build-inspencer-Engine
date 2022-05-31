f8352ed633881c1913c3ddaacb81c6c79e4f00e0
Gustavo S Simões <72417648+phikill@users.noreply.github.com>
2022-05-31 10:28:47 -0300

    Update mainMethod.java

--- mainEngine/SourceCode/engine/base/game/mainMethod.java
+++ mainEngine/SourceCode/engine/base/game/mainMethod.java
@@ -7,8 +7,8 @@ public class mainMethod
 {
 	public static void main(String[] args)
 	{
-		CoreEngine engine = new CoreEngine(1000, 600, 60, new TestGame());
-		engine.CreateWindow("Build Inspencer");
+		CoreEngine engine = new CoreEngine(1000, 600, 1000, new TestGame());
+		engine.CreateWindow("Build Inspencer" + " FPS");
 		engine.Start();
 	}
 }
