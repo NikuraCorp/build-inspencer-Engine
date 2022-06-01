1b071963e99924eda598c18bde26b60005667bc3
Gustavo S Simões <72417648+phikill@users.noreply.github.com>
2022-06-01 20:46:35 -0300

    Update and rename server to runServer.java

---
+++ mainServer/runServer.java
@@ -0,0 +1,4 @@
+
+/*calma se você leu isso é porque estás curioso mas  
+   preciso implementar o terreno e entidades para que o servidor carregue 
+   então espere mais um pouco para que servidores sejam criados*/
