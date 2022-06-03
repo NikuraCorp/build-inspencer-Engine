23b9e3ede9e4f79ce23c04b16a2f50e95fe8c44f
Gustavo S Simões <72417648+phikill@users.noreply.github.com>
2022-06-03 19:00:49 -0300

    Update inGame.java

--- mainEngine/SourceCode/engine/base/game/inGame.java
+++ mainEngine/SourceCode/engine/base/game/inGame.java
@@ -9,8 +9,12 @@ public class inGame extends Game
 {
 	public void Init()
 	{
+		/*Models*/
 		
+		Mesh mesh2 = new Mesh("plane2.obj");
 		Mesh mesh = new Mesh("plane3.obj");
+		
+		/*Textures */
 		Material corpoMaterial = new Material(new Texture("corpo.png"), 0, 0,
 			new Texture("corpo_normal.png"), new Texture("corpo_disp.png"), 0.03f, -0.5f);
 		
@@ -26,20 +30,27 @@ public class inGame extends Game
 		Material stone = new Material(new Texture("stone.jpg"), 1, 8,
 				new Texture("stone_normal.jpg"), new Texture("stone_disp.jpg"), 0.04f, -1.0f);
 		
-		Material gradisu = new Material(new Texture("gradisu.jpg"), 0, 0,
-				new Texture("plane_normal.png"), new Texture("bricks2_disp.jpg"), 0.03f, -0.5f);
+		Material gradisu = new Material(new Texture("devs/gradisu.jpg"), 0, 0,
+				new Texture("biengine/default_normal.jpg"), new Texture("biengine/default_disp.png"), 0.03f, -0.5f);
 
 		Mesh tempMesh = new Mesh("player.obj");
 
 		MeshRenderer meshRenderer = new MeshRenderer(mesh, brick);
-		MeshRenderer planeGradisu = new MeshRenderer(mesh, gradisu);
+		MeshRenderer meshRenderer2 = new MeshRenderer(mesh, brick);
+		MeshRenderer planeGradisu = new MeshRenderer(mesh2, gradisu);
+	
+		
+		GameObject planeObject2 = new GameObject();
+		planeObject2.AddComponent(meshRenderer2);
+		planeObject2.GetTransform().GetPos().Set(16, 1, 8);
+		
 		GameObject planeObject = new GameObject();
 		planeObject.AddComponent(meshRenderer);
-		planeObject.GetTransform().GetPos().Set(0, -1, 5);
+		planeObject.GetTransform().GetPos().Set(0, 0, 5);
 		
 		GameObject planeWall = new GameObject();
 		planeWall.AddComponent(planeGradisu);
-		planeWall.GetTransform().GetPos().Set(0, 8, 5);
+		planeWall.GetTransform().GetPos().Set(15, 8, 30);
 
 		GameObject directionalLightObject = new GameObject();
 		DirectionalLight directionalLight = new DirectionalLight(new Vector3f(0,0,3), 0.2f);
@@ -49,27 +60,29 @@ public class inGame extends Game
 		GameObject pointLightObject = new GameObject();
 		pointLightObject.AddComponent(new PointLight(new Vector3f(3, 3, 5), 0.4f, new Attenuation(0, 0, 1)));
 
-		SpotLight spotLight = new SpotLight(new Vector3f(0,1,1), 0.4f,
+		SpotLight spotLight = new SpotLight(new Vector3f(0,1,1), 0.6f,
 				new Attenuation(0,0,0.1f), 0.7f);
 
 		GameObject spotLightObject = new GameObject();
 		spotLightObject.AddComponent(spotLight);
 
 		spotLightObject.GetTransform().GetPos().Set(1, 3, 5);
-		spotLightObject.GetTransform().SetRot(new Quaternion(new Vector3f(2, 1, 0), (float) Math.toRadians(80.0f)));
+		spotLightObject.GetTransform().SetRot(new Quaternion(new Vector3f(2, 1, 0), (float) Math.toRadians(60.0f)));
 
 		AddObject(planeObject);
+		AddObject(planeObject2);
 		AddObject(directionalLightObject);
 		AddObject(pointLightObject);
 		AddObject(spotLightObject);
 		AddObject(planeWall);
+
 
 		GameObject testMesh3 = new GameObject().AddComponent(new LookAtComponent()).AddComponent(new MeshRenderer(tempMesh, corpoMaterial));
 
 		AddObject(
 				//AddObject(
 				new GameObject().AddComponent(new FreeLook(0.3f)).AddComponent(new FreeMove(2.5f))
-						.AddComponent(new Camera(new Matrix4f().InitPerspective((float) Math.toRadians(70.0f),
+						.AddComponent(new Camera(new Matrix4f().InitPerspective((float) Math.toRadians(60.0f),
 								(float) Window.GetWidth() / (float) Window.GetHeight(), 0.02f, 1000.0f))));
 
 		AddObject(testMesh3);
